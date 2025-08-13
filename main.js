af8db0b4b262341925144f39dc9dfcd37493caee
rabbit <night31916@gmail.com>
2025-08-13 23:52:34 +0800

    update account

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+// form
+document.getElementById('resetBtn').addEventListener('click', function () {
+    const form = document.getElementById('data');
+    form.reset();
+
+    // 額外清空所有欄位
+    form.querySelectorAll('input').forEach(input => {
+        if (input.type !== 'radio' && input.type !== 'checkbox' && input.type !== 'file') {
+            input.value = '';
+        }
+        if (input.type === 'radio' || input.type === 'checkbox') {
+            input.checked = false;
+        }
+    });
+});
